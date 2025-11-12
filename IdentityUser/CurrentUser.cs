6dcb2ca9fd6e029f39710137a80fb39b8348fc09
minhanglin-ux <minhang.lin@sarria.salesians.cat>
2025-11-12 17:57:49 +0100

    Creaci'on de la clase CurrentUser y su User

---
+++ IdentityUser/CurrentUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityUser
+{
+    public static class CurrentUser
+    {
+        public static User MainUser { get; set; }
+    }
+
+    public class User
+    {
+        public int idUser { get; set; }
+        public string CodeUser { get; set; }
+        public string UserName { get; set; }
+        public string Login { get; set; }
+        //Path Image
+        public string Photo { get; set; }
+        public int AccesLevel { get; set; }
+        public string DescRank { get; set; }]
+    }
+}
