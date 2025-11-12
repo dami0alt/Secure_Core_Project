1610de5e1c8b64bf3320db8f6535142ad122eeca
minhanglin-ux <minhang.lin@sarria.salesians.cat>
2025-11-12 16:31:01 +0100

    feature: Creación del formulario FrmConfirmPassword
    Creación del formulario FrmConfirmPassword - MInhang

---
+++ JobHuntersSystem/frmConfirmPassword.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace JobHuntersSystem
+{
+    partial class frmConfirmPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirmPassword = new System.Windows.Forms.Button();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.lblNewPassword = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConfirmPassword
+            // 
+            this.btnConfirmPassword.Location = new System.Drawing.Point(585, 281);
+            this.btnConfirmPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConfirmPassword.Name = "btnConfirmPassword";
+            this.btnConfirmPassword.Size = new System.Drawing.Size(96, 36);
+            this.btnConfirmPassword.TabIndex = 10;
+            this.btnConfirmPassword.Text = "Done";
+            this.btnConfirmPassword.UseVisualStyleBackColor = true;
+            this.btnConfirmPassword.Click += new System.EventHandler(this.btnConfirmPassword_Click);
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmPassword.Location = new System.Drawing.Point(76, 212);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(189, 23);
+            this.lblConfirmPassword.TabIndex = 9;
+            this.lblConfirmPassword.Text = "Confirm Password:";
+            // 
+            // lblNewPassword
+            // 
+            this.lblNewPassword.AutoSize = true;
+            this.lblNewPassword.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewPassword.Location = new System.Drawing.Point(157, 126);
+            this.lblNewPassword.Name = "lblNewPassword";
+            this.lblNewPassword.Size = new System.Drawing.Size(108, 23);
+            this.lblNewPassword.TabIndex = 8;
+            this.lblNewPassword.Text = "Password:";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(290, 212);
+            this.txtConfirmPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(391, 22);
+            this.txtConfirmPassword.TabIndex = 7;
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Location = new System.Drawing.Point(290, 129);
+            this.txtNewPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.PasswordChar = '*';
+            this.txtNewPassword.Size = new System.Drawing.Size(391, 22);
+            this.txtNewPassword.TabIndex = 6;
+            // 
+            // frmConfirmPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(79)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConfirmPassword);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.lblNewPassword);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtNewPassword);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(143)))), ((int)(((byte)(82)))));
+            this.Name = "frmConfirmPassword";
+            this.Text = "FrmConfirmPassword";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirmPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.Label lblNewPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.TextBox txtNewPassword;
+    }
+}
---
+++ JobHuntersSystem/frmConfirmPassword.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HashUtils;
+using ComponentesDeAcceso;
+
+namespace JobHuntersSystem
+{
+    public partial class frmConfirmPassword : Form
+    {
+        public frmConfirmPassword(string Login)
+        {
+            InitializeComponent();
+            userName = Login;
+        }
+
+        private string userName;
+        private clsHash hash = new clsHash();
+        private BaseDeDades bd = new BaseDeDades();
+        private string salt;
+        private string passHash;
+
+        private bool AreEqual()
+        {
+            if (txtConfirmPassword.Text.Equals(txtNewPassword.Text))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void HashPassword(string password)
+        {
+            salt = hash.CreateSalt();
+            passHash = hash.CreatePassword(password, salt);
+        }
+
+        private void UpdatePass()
+        {
+            string query = "select Password, hash, Login" +
+                "from Users" +
+                $"where Login = '{userName}'";
+            DataSet dataSet = bd.PortarPerConsulta(query);
+            foreach (DataRow row in dataSet.Tables[0].Rows)
+            {
+                if (row["Login"].ToString() == userName)
+                {
+                    row["hash"] = salt;
+                    row["Password"] = passHash;
+                }
+            }
+            bd.Actualitzar(query, dataSet);
+        }
+
+        private void ConfirmPassword()
+        {
+            if (!AreEqual())
+            {
+                string message = "Passwords need to be equals";
+                MessageBox.Show(message);
+            }
+            else
+            {
+                string pass = txtNewPassword.Text;
+                HashPassword(pass);
+                UpdatePass();
+                this.Dispose();
+            }
+        }
+
+        private void btnConfirmPassword_Click(object sender, EventArgs e)
+        {
+            ConfirmPassword();
+        }
+    }
+}
